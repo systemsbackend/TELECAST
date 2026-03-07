5712b5b6f7a22a754418dc760b63715457c43790
Backend Systems <systemsbackend@gmail.com>
2026-03-08 01:59:40 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL','mongodb+srv://systemsbackend_db_user:xetwfAxvuX44Hyef@cluster0.rcrbgdt.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL','mongodb+srv://systemsbackend_db_user:xetwfAxvuX44Hyef@embuonline.fxtnmak.mongodb.net/?appName=embuonline'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL','embuonlinetv'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
